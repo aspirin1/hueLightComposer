3f9df7aaa34fdc5322f4b8175dcc263ed3526691
dressiermaus <chrissy@abwesend.de>
2016-03-04 19:49:07 +0100

    design.key = "light", falls LS design fehlt

--- www/js/controllers/layout/BodyCtrl.js
+++ www/js/controllers/layout/BodyCtrl.js
@@ -7,13 +7,16 @@ define(function () {
 
         $scope.theme = function () {
             var design = ConfigService.getDesign();
-            if (angular.isDefined(design) && angular.isDefined(design.key))
-                return design.key;
-
+            // RALF das musste noch richtig machen: Wenn der Key ls.design im Local Storgage aus irgendeinem Grund fehlt,
+            // ist Alles für immer doof & man bekommt 12.000 Fehler(ca.), weil design.key natürlich nicht existiert;
+            // hab hier nur mal schnell die Abfragen umgedreht und es so hingepfuscht, dass ich daheim Licht machen kann =^.^=
+            // Noch einmal die original hue-app für irgendwas zu verwenden hätte ich nicht überlebt. Es war ein absoluter Notfall.
             if (angular.isUndefined(design) || design === null || angular.isUndefined(design.key) || typeof (design) === "string") {
                 design = {};
                 design.key = "light";
             }
+            if (angular.isDefined(design) && angular.isDefined(design.key))
+                return design.key;
 
             return design.key;
         };
